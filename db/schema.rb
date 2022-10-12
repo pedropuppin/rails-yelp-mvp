c3acdaa5a01541b9e091563c3b6d57a7b531fa16
pedro puppin <puppin.pedro@gmail.com>
2022-10-12 13:31:37 -0300

    ex2

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_003118) do
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
