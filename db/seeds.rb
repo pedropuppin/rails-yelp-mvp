9762e6d8c8a3ec6eaa690470c5a688a64ba8df54
pedro puppin <puppin.pedro@gmail.com>
2022-10-12 14:41:08 -0300

    ex2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning the database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Base.numerify('+55 (##)#####-####'),
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
